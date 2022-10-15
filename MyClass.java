65ffc303bc5bba07ce488aa6c74ff97944404432
Luiz Henrique <81529238+IkkyLuiz@users.noreply.github.com>
2022-10-15 15:06:13 -0300

    Add files via upload
    Atualizações do código utlizando String e concatenação para exibir meu nome.

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    
+public static void main (String [] args) {
+    String primeiroNome = "Luiz";
+    String segundoNome = "Henrique";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}
+public static String nomeCompleto(String primeiroNome, String segundoNome){
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+}
